ae1aad7c6e7156976f633e6c32e51f4c5f313451
tkhsykk <tkhsykk@gmail.com>
2025-11-15 07:47:24 +0900

    足りないスタイルを追加、修正

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@
  */
 
 import gulp from 'gulp';
-import dartSass from 'sass';
+import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
